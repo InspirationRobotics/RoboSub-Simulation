d465ea318847d06c85849ed3cdc0c43b5baf46aa
saxenapratham6 <40923393+saxenapratham6@users.noreply.github.com>
2021-06-06 10:53:46 -0700

    Add files via upload

---
+++ TransdecEnvironment/Assets/Editor/PlaceableEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Placeable)), CanEditMultipleObjects]
+public class PlaceableEditor : Editor
+{
+    SerializedProperty debugModeProperty;
+    SerializedProperty probingVectorProperty;
+
+    void OnEnable()
+    {
+        debugModeProperty = serializedObject.FindProperty("debugMode");
+        probingVectorProperty = serializedObject.FindProperty("probingVector");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Auto Adjust")) {
+            foreach (Object target in targets)
+            {
+                Undo.RegisterCompleteObjectUndo(target, "Auto adjustment of " + target.name);
+                ((Placeable)target).AutoAdjust();
+            }
+        }
+        
+        if (debugModeProperty.boolValue)
+        {
+            EditorGUILayout.HelpBox("There is a white line showing the probing vector and yellow line showing what is the allowed distance in this direction.", MessageType.Info);
+            EditorGUILayout.PropertyField(probingVectorProperty, new GUIContent("Probing Vector"));
+        }
+        
+        serializedObject.ApplyModifiedProperties();
+    }
+}
